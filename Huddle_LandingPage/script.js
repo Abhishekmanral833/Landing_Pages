5e6654e8e9615cacc8aa16f8d715f92056a048f0
Abhishek_Manral <abhishekmanral2003@gmail.com>
2023-08-23 10:21:57 +0530

    first landing page create using html,css,js

---
+++ Huddle_LandingPage/script.js
@@ -0,0 +1,9 @@
+let registerButton = document.querySelector("h5");
+registerButton.addEventListener("click",function(){
+    registerButton.textContent = "Successfully Registered!";
+});
+
+let buttonSize = document.querySelector("button");
+buttonSize.addEventListener("click",function(){
+    buttonSize.style.width = "150px";
+});
